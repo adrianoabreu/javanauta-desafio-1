f7417a20d877afe23119c7eeac65b9702787a97c
Nelio Alves <adriano.luis.machado2013@hotmail.com>
2025-11-17 18:49:32 -0300

    Implementando Conversao entre DTO e Entity com mapStruct e implementando classe GlobalExceptionHandler

--- src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoConverter.java
+++ src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoConverter.java
@@ -5,12 +5,25 @@ import com.luizalebs.comunicacao_api.api.dto.ComunicacaoOutDTO;
 import com.luizalebs.comunicacao_api.infraestructure.entities.ComunicacaoEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @AllArgsConstructor
 @Component
-public class ComunicacaoConverter {
+@Mapper(componentModel = "spring")
+//public class ComunicacaoConverter {
+public interface ComunicacaoConverter {
 
-    public ComunicacaoEntity paraEntity(ComunicacaoInDTO dto) {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataHoraenvio", target = "dataHoraenvio")
+    @Mapping(source = "emailDestinatario", target = "emailDestinatario")
+    @Mapping(source = "nomeDestinatario", target = "nomeDestinatario")
+    @Mapping(source = "mensagem", target = "mensagem")
+    @Mapping(source = "modoDeEnvio", target = "modoDeEnvio")
+    @Mapping(source = "statusEnvio", target = "statusEnvio")
+    @Mapping(source = "telefoneDestinatario", target = "telefoneDestinatario")
+    ComunicacaoEntity paraEntity(ComunicacaoInDTO dto)
+/*    public ComunicacaoEntity paraEntity(ComunicacaoInDTO dto) {
         return ComunicacaoEntity.builder()
                 .dataHoraenvio(dto.getDataHoraEnvio())
                 .emailDestinatario(dto.getEmailDestinatario())
@@ -21,7 +34,18 @@ public class ComunicacaoConverter {
                 .telefoneDestinatario(dto.getTelefoneDestinatario())
                 .build();
     }
+*/
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataHoraenvio", target = "dataHoraenvio")
+    @Mapping(source = "emailDestinatario", target = "emailDestinatario")
+    @Mapping(source = "nomeDestinatario", target = "nomeDestinatario")
+    @Mapping(source = "mensagem", target = "mensagem")
+    @Mapping(source = "modoDeEnvio", target = "modoDeEnvio")
+    @Mapping(source = "telefoneDestinatario", target = "telefoneDestinatario")
+    @Mapping(source = "statusEnvio", target = "statusEnvio")
+    ComunicacaoOutDTO paraDTO(ComunicacaoEntity entity)
+/*
     public ComunicacaoOutDTO paraDTO(ComunicacaoEntity entity) {
         return ComunicacaoOutDTO.builder()
                 .dataHoraEnvio(entity.getDataHoraenvio())
@@ -33,4 +57,6 @@ public class ComunicacaoConverter {
                 .statusEnvio(entity.getStatusEnvio())
                 .build();
     }
+*/
 }
+
